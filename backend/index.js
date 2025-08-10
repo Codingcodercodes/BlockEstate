06ed534f227b3c2a309731c3f767e7e6ee104104
P Charmi Reddy <163008537+Codingcodercodes@users.noreply.github.com>
2025-08-10 20:04:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+
+
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -7,7 +9,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 // Middleware
 app.use(cors());
 app.use(express.json());
