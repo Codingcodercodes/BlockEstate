dff625e17db67dad070606320c82fdef8df50852
P Charmi Reddy <163008537+Codingcodercodes@users.noreply.github.com>
2025-11-20 21:50:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,8 @@ const fs = require('fs');
 const axios = require('axios');
 const session = require('express-session');
 const cors = require('cors');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// --- Middleware ---
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
